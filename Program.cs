9c217102d8666e4a72a8c0e3ee34769b27979734
Myaroslavtsev <myaroslavtsev@yandex.ru>
2025-07-09 15:42:16 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using ShapeData;
+
+namespace ShapeReplicator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+                Console.WriteLine("Hello World!");
+
+            ShapeData.PartReplication.
+        }
+    }
+}
